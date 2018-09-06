1bad81d412f29aa1dd38c2badb9f84fa5a090c45
Leonardo <liogothicvampire@gmail.com>
2018-09-06 12:32:15 -0500

    Creating a new Project to develop a Java connector

---
+++ buildJavaConnectionWithNeo/src/main/java/org/liobouchan/buildJavaConnectionWithNeo/App.java
@@ -0,0 +1,13 @@
+package org.liobouchan.buildJavaConnectionWithNeo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

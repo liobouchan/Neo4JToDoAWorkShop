bc9b1debae3a8251df7138bd7cf701f530f749eb
Leonardo <liogothicvampire@gmail.com>
2018-09-11 12:57:32 -0500

    creating the PathResult

---
+++ buildNeo4jTreeDecision/src/main/java/org/liobouchan/buildNeo4jTreeDecision/results/PathResult.java
@@ -0,0 +1,11 @@
+package org.liobouchan.buildNeo4jTreeDecision.results;
+
+import org.neo4j.graphdb.Path;
+
+public class PathResult {
+    public final Path path;
+
+    public PathResult(Path path) {
+        this.path = path;
+    }
+}

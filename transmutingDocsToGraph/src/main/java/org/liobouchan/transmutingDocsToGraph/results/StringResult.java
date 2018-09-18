5d22aed91106a32f1739bcffb7a1cc64689dc82b
Leonardo <liogothicvampire@gmail.com>
2018-09-18 13:14:00 -0500

    Creating the stringresult for the schema

---
+++ transmutingDocsToGraph/src/main/java/org/liobouchan/transmutingDocsToGraph/results/StringResult.java
@@ -0,0 +1,11 @@
+package org.liobouchan.transmutingDocsToGraph.results;
+
+public class StringResult {
+	public final static StringResult EMPTY = new StringResult(null);
+	
+	public final String value;
+	
+	public StringResult(String value) {
+		this.value = value;
+	}
+}
